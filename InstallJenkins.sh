e07a83f44bb28b1adfd8d48858a5d0d36f194d47
mototvtv55 <139687798+mototvtv55@users.noreply.github.com>
2023-07-20 20:55:25 +0530

    Create InstallJenkins.sh

---
+++ InstallJenkins.sh
@@ -0,0 +1,20 @@
+
+
+#!/bin/sh
+
+sudo su 
+
+apt-get update
+
+apt install openjdk-17-jre
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+ apt-get update
+ apt-get install jenkins
+
+
+ 
