07f9fd0c7c8a69f85335bd1b2b752af968af7c09
mototvtv55 <139687798+mototvtv55@users.noreply.github.com>
2023-07-21 23:35:37 +0530

    Update InstallJenkins.sh
    -y

--- InstallJenkins.sh
+++ InstallJenkins.sh
@@ -6,7 +6,7 @@ sudo su
 
 apt-get update
 
-apt install openjdk-17-jre
+apt install -y openjdk-17-jre
 
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
@@ -14,7 +14,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
  apt-get update
- apt-get install jenkins
+ apt-get install -y jenkins
 
 
  
